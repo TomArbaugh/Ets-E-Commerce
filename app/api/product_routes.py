7069746b166d948a0d9a2b83beed1aeb805ac444
elya-le <elyaj.le@gmail.com>
2024-07-01 15:01:39 -0500

    product route - testing get all and get by product id

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -6,11 +6,26 @@ product_routes = Blueprint('products', __name__)
 
 @product_routes.route('/')
 def get_products():
+    """
+    Get all products
+    """
     all_products = Product.query.all()
     return {"products" : [product.to_dict() for product in all_products]}
 
 @product_routes('/<int:product_id>')
 def get_product_by_id(product_id):
+    """
+    Get a product by ID
+    """
     product = Product.query.get(id)
+    if product is None:
+      return {'errors': {'message': 'Product not found'}}, 404
+    return product.to_dict()
+
+
+
+
+
+  
     
 
