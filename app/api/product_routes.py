00c2c8e255eddcd8864d79d2aa8ab4628fb3bece
elya-le <elyaj.le@gmail.com>
2024-07-01 15:53:49 -0500

    problem solving for get current user

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect
 from flask_login import login_required
-from app.models import Product
+from app.models import Product, db
 
 product_routes = Blueprint('products', __name__)
 
@@ -10,7 +10,7 @@ def get_products():
     Get all products
     """
     all_products = Product.query.all()
-    return {"products" : [product.to_dict() for product in all_products]}
+    return {"products": [product.to_dict() for product in all_products]}
 
 @product_routes.route('/<int:product_id>')
 def get_product_by_id(product_id):
@@ -22,7 +22,35 @@ def get_product_by_id(product_id):
       return {'errors': {'message': 'Product not found'}}, 404
     return product.to_dict()
 
-
+@product_routes.route('/', methods=['GET', 'POST'])
+@login_required
+def creat_product():
+    """
+    Create product
+    """
+    form = ProductForm()
+    if form.validate_on_sumbit():
+        new_product = Product(
+            owner_id=current_user.id,
+            name=form.data['Name'],
+            category=form.data['Category'],
+            description=form.data['Description'],
+            price=form.data['Price'],
+            stock=form.data['Stock']
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect('/')
+    
+@product_routes.route('/current')
+@login_required
+def get_user_products():
+    """
+    Get all product by current logged-in user
+    """
+    user_id = current_user.id
+    user_products = Product.query.filter_by(owner_id=user_id).all
+    return {"products": [product.to_dict() for product in user_products]}
 
 
 
