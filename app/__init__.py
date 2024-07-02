ac2f552c0caada25b10e4bdf0bf4383179186681
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2024-07-01 17:49:39 -0700

    added blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -31,6 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(product_routes, url_prefix='/api/products')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 db.init_app(app)
 Migrate(app, db)
 
