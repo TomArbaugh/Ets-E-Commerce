34b9d90c39c19cda27ad40e5187f1eaf950e2193
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2024-07-05 08:43:00 -0700

    commit build to main

--- react-vite/src/components/CancelOrder/CancelOrder.jsx
+++ react-vite/src/components/CancelOrder/CancelOrder.jsx
@@ -1,33 +1,34 @@
 // import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 function CancelOrder() {
-    const { orderId } = useParams();
+    // const { orderId } = useParams();
 
-    // const userId = useSelector(state => state.session.user.id)
+    // // const userId = useSelector(state => state.session.user.id)
 
-    const setState = async (e) => {
-        e.preventDefault()
-        try {
-            const fetchAllOrders = await fetch(`/api/orders`);
-            const fetchedOrders = await fetchAllOrders.json();
-            // console.log("FETCHED ORDERS: ", fetchedOrders[0])
-            const order = fetchedOrders.find((order) => order.id == orderId)
-            // console.log("ORDER: ", order)
-            if (order.status === 'pending') {
-                const deleteFetch = await fetch(`/api/orders/${orderId}/delete-order`, {
-                    method: 'DELETE'
-                })
-                // const result = await deleteFetch.json()
-                // console.log(result)
-            }
-        } catch (err) {
-            console.error("Request Error:", err);
-        }
-    }
+    // const setState = async (e) => {
+    //     e.preventDefault()
+    //     try {
+    //         const fetchAllOrders = await fetch(`/api/orders`);
+    //         const fetchedOrders = await fetchAllOrders.json();
+    //         // console.log("FETCHED ORDERS: ", fetchedOrders[0])
+    //         const order = fetchedOrders.find((order) => order.id == orderId)
+    //         // console.log("ORDER: ", order)
+    //         if (order.status === 'pending') {
+    //             // const deleteFetch = await fetch(`/api/orders/${orderId}/delete-order`, {
+    //             //     method: 'DELETE'
+    //             })
+    //             // const result = await deleteFetch.json()
+    //             // console.log(result)
+    //         }
+    //     } catch (err) {
+    //         console.error("Request Error:", err);
+    //     }
+    // }
 
     return (
-        <button onClick={setState}>Cancel Order</button>
+        <h2>Hi</h2>
+        // <button onClick={setState}>Cancel Order</button>
     )
 }
 
