b693143e74e5c83989b7d001344374bd58c438c0
Elya <elyaj.le@gmail.com>
2024-07-05 04:06:06 -0500

    elyas redux review before branching to will-the-real-sc-please-stand-up

--- react-vite/src/redux/reviews.js
+++ react-vite/src/redux/reviews.js
@@ -1,16 +1,10 @@
 const GET_REVIEWS = 'getReveiws'
-<<<<<<< HEAD
 
-=======
->>>>>>> be9acb02c14570ac3cdab2f6d57f105c29bcb5a1
 const getReviews = (productId, data) => ({
     type: GET_REVIEWS,
     payload: data
 })
-<<<<<<< HEAD
 
-=======
->>>>>>> be9acb02c14570ac3cdab2f6d57f105c29bcb5a1
 export const getReviewsByProductId = (productId) => async (dispatch) => {
     const response = await fetch(`/api/reviews/${productId}/reviews`)
     if (response.ok) {
@@ -22,13 +16,9 @@ export const getReviewsByProductId = (productId) => async (dispatch) => {
     }
     
 }
-<<<<<<< HEAD
 
 const initialState = {}
 
-=======
-const initialState = {}
->>>>>>> be9acb02c14570ac3cdab2f6d57f105c29bcb5a1
 function reviewsReducer(state = initialState, action) {
     switch (action.type) {
       case GET_REVIEWS:
@@ -37,8 +27,5 @@ function reviewsReducer(state = initialState, action) {
         return state;
     }
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> be9acb02c14570ac3cdab2f6d57f105c29bcb5a1
 export default reviewsReducer
