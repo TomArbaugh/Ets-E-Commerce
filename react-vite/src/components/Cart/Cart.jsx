12d5da926a3add220e1eb875cc65c66c0ee10e3b
Hui Wen <lovelyelsa@macbook-air-10.lan>
2024-07-05 15:04:53 -0700

    remove delete cart item modal, rewrite

--- react-vite/src/components/Cart/Cart.jsx
+++ react-vite/src/components/Cart/Cart.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCartItems} from '../../redux/cart';
-import OpenModalButton from '../OpenModalButton';
-import DeleteCartItemModal from './DeleteCartItemModal'
 import './Cart.css';
 
 const Cart = () => {
@@ -28,10 +26,7 @@ const Cart = () => {
                <div id='small-box'>
                  <p>{item.product.name}</p>
                  <p>Quantity: {item.quantity}</p>
-                 <OpenModalButton id='delete-modal-button'
-                        buttonText='Delete'
-                        modalComponent={<DeleteCartItemModal catItemId={item.id} />}
-                      />
+                 <p>Delete</p>
                </div>
          ) : (
                <p>Product details not available</p>
