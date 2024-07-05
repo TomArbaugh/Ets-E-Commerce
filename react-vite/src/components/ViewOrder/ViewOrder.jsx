78682f57e00c34a7555dd3681593e93a354e0ed1
Aaron <aaronipollock@gmail.com>
2024-07-04 21:39:59 -0700

    back

--- react-vite/src/components/ViewOrder/ViewOrder.jsx
+++ react-vite/src/components/ViewOrder/ViewOrder.jsx
@@ -1,9 +1,17 @@
 import { useSelector } from "react-redux"
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { thunkGetAllProducts } from "../../redux/products";
 
 
 
 function ViewOrder() {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(thunkGetAllProducts());
+      }, [dispatch]);
 
     const user = useSelector((state) => state.session.user)
     const products = useSelector((state) => state.products.allProducts || []);
@@ -21,30 +29,31 @@ function ViewOrder() {
 
     }
     fetchOrders()
+
+
+
     const ordersArr = []
     orders.map((order) => (
 
-        
-
         order.products_ordered.map((order_item) => {
-
+            const order = []
             const orderItemObj = {}
 
             const product = products.find((product) => order_item.product_id === product.id)
 
-            const id = order_item.order_id
-                
+            // const id = order_item.order_id
+
             orderItemObj[name] = product.name,
             orderItemObj[price] = product.price,
             orderItemObj[quantity] = order_item.quanity,
             orderItemObj[total] = order_item.quantity * product.price
-        
 
+            order.push(orderItemObj)
+            ordersArr.push(order)
         })
 
-
     ))
- 
+    console.log("ORDERSARRAY: ", ordersArr)
 
     return (
         <>
