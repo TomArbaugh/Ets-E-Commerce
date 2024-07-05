065b93ffb58586b1ed486cff564762a876ae7656
Aaron <aaronipollock@gmail.com>
2024-07-05 10:44:58 -0700

    deletes after click and before refresh

--- react-vite/src/components/DeleteReviewModal/DeleteReviewModal.jsx
+++ react-vite/src/components/DeleteReviewModal/DeleteReviewModal.jsx
@@ -1,18 +1,25 @@
 import { useModal } from "../../context/Modal";
 import DeleteReview from "../DeleteReview/DeleteReview";
 import './DeleteReviewModal.css'
-function DeleteReviewModal() {
+// import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
+function DeleteReviewModal({productId}) {
+    const navigate = useNavigate();
+    // const { productId } = useParams();
     const { closeModal } = useModal();
+    console.log("PRODUCT ID: ", productId)
     function onSubmit(e) {
         e.preventDefault()
         closeModal()
-
     }
+
+    navigate(`/products/${productId}`)
+
     return (
         <form>
         <h1>Are you sure you want to delete?</h1>
-        <button 
+        <button
         type='submit'
         onClick={onSubmit}
         >No</button>
@@ -22,7 +29,7 @@ function DeleteReviewModal() {
         >
         <DeleteReview id="delete-review"/>
         </button>
-       
+
         </form>
     )
 }
