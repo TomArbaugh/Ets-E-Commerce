2d4101df044c2da3fa68f86a7f9ae713c5cddfe2
Aaron <aaronipollock@gmail.com>
2024-07-05 12:34:35 -0700

    post-lunch

--- react-vite/src/components/ViewOrder/ViewOrder.jsx
+++ react-vite/src/components/ViewOrder/ViewOrder.jsx
@@ -1,91 +1,91 @@
-// import { useSelector } from "react-redux"
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { thunkGetAllProducts } from "../../redux/products";
-// import { useState } from "react";
+import { useSelector } from "react-redux"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { thunkGetAllProducts } from "../../redux/products";
+import { useState } from "react";
 
 
 function ViewOrder() {
 
-    // const dispatch = useDispatch()
-    // const [orders, setOrders] = useState([])
+    const dispatch = useDispatch()
+    const [orders, setOrders] = useState([])
 
-    // useEffect(() => {
-    //     dispatch(thunkGetAllProducts());
-    //   }, [dispatch]);
+    useEffect(() => {
+        dispatch(thunkGetAllProducts());
+      }, [dispatch]);
 
-    // const user = useSelector((state) => state.session.user)
-    // // const products = useSelector((state) => state.products.allProducts || []);
-
-    
-    
-    // let response;
-    // const fetchOrders = async () => {
-
-    //     try {
-    //         response = await fetch(`/api/orders/`)
-    //         const data = await response.json()
-    //         setOrders(data)
-    //         // console.log("27", orders)
-    //     } catch (err) {
-    //         console.error('Request Error:', err);
-    //     }
-        
-    // }
-    // useEffect(() => {
-    //     fetchOrders()
-    // }, [dispatch])
-    
+    const user = useSelector((state) => state.session.user)
+    const products = useSelector((state) => state.products.allProducts || []);
 
 
-    // console.log(orders)
 
-    
-//     const ordersArr = []
-//     orders ? orders.map((order) => {
+    let response;
+    const fetchOrders = async () => {
 
-//         // console.log(order)
+        try {
+            response = await fetch(`/api/orders/`)
+            const data = await response.json()
+            setOrders(data)
+            // console.log("27", orders)
+        } catch (err) {
+            console.error('Request Error:', err);
+        }
 
-//         // order.products_ordered.map((order_item) => {
-//         //     const order = []
-//         //     const orderItemObj = {}
+    }
+    useEffect(() => {
+        fetchOrders()
+    }, [dispatch])
 
-//         //     const product = products.find((product) => order_item.product_id === product.id)
 
-//         //     // const id = order_item.order_id
 
-//         //     orderItemObj[name] = product.name,
-//         //     orderItemObj.price = product.price,
-//         //     orderItemObj.quantity = order_item.quanity,
-//         //     orderItemObj.total = order_item.quantity * product.price
+    console.log(orders)
 
-//         //     order.push(orderItemObj)
-//         //     ordersArr.push(order)
-//         // })
 
-// }) : null
-    // console.log("ORDERSARRAY: ", ordersArr)
+    const ordersArr = []
+    orders ? orders.map((order) => {
 
-    // if (!orders) return "no orders"
+        console.log("ORDER: ", order)
+
+        order.products_ordered.map((order_item) => {
+            const order = []
+            const orderItemObj = {}
+
+            const product = products.find((product) => order_item.product_id === product.id)
+
+            // const id = order_item.order_id
+
+            orderItemObj.name = order_item.name,
+            orderItemObj.price = order_item.price,
+            // orderItemObj.quantity = order_item.quanity,
+            // orderItemObj.total = order_item.quantity * order_item.price
+
+            order.push(orderItemObj)
+            ordersArr.push(order)
+        })
+
+}) : null
+    console.log("ORDERSARRAY: ", ordersArr)
+
+    if (!orders) return "no orders"
     return (
         <>
-            {/* <h1>Orders for {user.first_name}</h1>
-            {orders ? orders.map((order) => (
+            <h1>Orders for {user.first_name}</h1>
+            {ordersArr ? ordersArr.map((order_items) => (
                 <>
-                    {order.products_ordered.map((order_items) => (
+                    {/* {order.products_ordered.map((order_items) => ( */}
                         <>
-                            <p>Order Id: {order_items.order_id}</p>
-                            <p>Product Id: {order_items.product_id}</p>
-                            <p>Quantity: {order_items.quantity}</p>
+                            {/* <p>Order Id: {order_items.order_id}</p> */}
+                            <p>Product Name: {order_items.name}</p>
+                            <p>Price: {order_items.price}</p>
                         </>
 
-                    ))}
-                    <p>Total: {order.total}</p>
+                    {/* ))} */}
+                    {/* <p>Total: {order.total}</p> */}
                     <p></p>
                     <p></p>
                     <p></p>
                 </>
-            )) : null} */}
+            )) : null}
 
         </>
     )
