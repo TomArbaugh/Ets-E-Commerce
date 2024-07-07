ac30ccd06f7e7c7eee2ea49899927adc1d0ec061
Elya <elyaj.le@gmail.com>
2024-07-07 16:45:48 -0500

    rerouting no dont delete review button

--- react-vite/src/components/DeleteReviewModal/DeleteReviewModal.jsx
+++ react-vite/src/components/DeleteReviewModal/DeleteReviewModal.jsx
@@ -1,36 +1,44 @@
 import { useModal } from "../../context/Modal";
 import DeleteReview from "../DeleteReview/DeleteReview";
-import './DeleteReviewModal.css'
-// import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom"
+import './DeleteReviewModal.css';
+import { useNavigate } from "react-router-dom";
 
-function DeleteReviewModal() {
+function DeleteReviewModal({ productId }) {
     const navigate = useNavigate();
-    // const { productId } = useParams();
     const { closeModal } = useModal();
 
-    function onSubmit(e) {
+    function handleNoClick(e) {
+        e.preventDefault();
+        closeModal();
+        navigate(`/products/${productId}`);
+    }
+
+    function handleYesClick(e) {
         e.preventDefault();
         closeModal();
         navigate(`/`);
     }
 
     return (
-        <form className="delete-review-modal" onSubmit={onSubmit}>
+        <form className="delete-review-modal">
             <h2>Are you sure you want to delete?</h2>
             <div className="delete-review-buttons">
-            <button
-                type='button'
-                onClick={onSubmit}
-                className="no-button"
-            >No</button>
-            <button
-                type='submit'
-                className="yes-button"
-            > <DeleteReview>
-                    Yes
-                </DeleteReview>
-            </button>
+                <button
+                    type='button'
+                    onClick={handleNoClick}
+                    className="no-button"
+                >
+                    No
+                </button>
+                <button
+                    type='submit'
+                    className="yes-button"
+                    onClick={handleYesClick}
+                >
+                    <DeleteReview>
+                        Yes
+                    </DeleteReview>
+                </button>
             </div>
         </form>
     );
