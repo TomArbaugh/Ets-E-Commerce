f028b59a2bc1af8c2e56d4891b14f77f8af41a7e
Aaron <aaronipollock@gmail.com>
2024-07-07 19:22:19 -0700

    trying

---
+++ react-vite/src/components/CancelOrder/CancelOrder.jsx
@@ -0,0 +1,37 @@
+import { useNavigate} from "react-router-dom";
+
+
+function CancelOrder({orderId}) {
+    // const {orderId} = useParams()
+    const navigate = useNavigate()
+
+    const setState = async (e) => {
+        e.preventDefault()
+        console.log("OrderId", orderId)
+        try {
+            const fetchAllOrders = await fetch(`/api/orders/`);
+            const fetchedOrders = await fetchAllOrders.json();
+            // console.log("FETCHED ORDERS: ", fetchedOrders)
+            const order = fetchedOrders.find((order) => order.id == orderId)
+            // console.log("ORDER: ", order)
+            if (order.status === 'Pending') {
+                const deleteFetch = await fetch(`/api/orders/${orderId}/delete-order`, {
+                    method: 'DELETE'
+                })
+                const result = await deleteFetch.json()
+                if (result) {
+                    navigate('/orders/view')
+                }
+            }
+        } catch (err) {
+            console.error("Request Error:", err);
+        }
+    }
+
+    return (
+
+        <button className='yes-button' onClick={setState}>Cancel Order</button>
+    )
+}
+
+export default CancelOrder;
